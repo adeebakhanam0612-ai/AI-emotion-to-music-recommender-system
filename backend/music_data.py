5c8c63df60a77c106672e9b5ae69424a2de076cd
adeebakhanam0612-ai <adeebakhanam0612@gmail.com>
2026-06-22 22:45:03 +0530

    Update music_data.py

--- backend/music_data.py
+++ backend/music_data.py
@@ -1,84 +1,93 @@
 music_recommendations = {
 
     "happy": [
-        {
-            "name": "Happy - Pharrell Williams",
-            "video": "https://www.youtube.com/watch?v=ZbZSe6N_BXs"
-        },
-        {
-            "name": "Shape of You - Ed Sheeran",
-            "video": "https://www.youtube.com/watch?v=JGwWNGJdvx8"
-        },
-        {
-            "name": "Blinding Lights - The Weeknd",
-            "video": "https://www.youtube.com/watch?v=4NRXx6U8ABQ"
-        }
+        {"name": "Happy - Pharrell Williams", "video": "https://www.youtube.com/watch?v=ZbZSe6N_BXs"},
+        {"name": "Can't Stop The Feeling - Justin Timberlake", "video": "https://www.youtube.com/watch?v=ru0K8uYEZWw"},
+        {"name": "Shape of You - Ed Sheeran", "video": "https://www.youtube.com/watch?v=JGwWNGJdvx8"},
+        {"name": "Uptown Funk - Bruno Mars", "video": "https://www.youtube.com/watch?v=OPf0YbXqDm0"},
+        {"name": "Levitating - Dua Lipa", "video": "https://www.youtube.com/watch?v=TUVcZfQe-Kw"},
+        {"name": "On Top of the World - Imagine Dragons", "video": "https://www.youtube.com/watch?v=w5tWYmIOWGk"},
+        {"name": "Good Time - Owl City", "video": "https://www.youtube.com/watch?v=H7HmzwI67ec"},
+        {"name": "Best Day Of My Life - American Authors", "video": "https://www.youtube.com/watch?v=Y66j_BUCBMY"},
+        {"name": "Count On Me - Bruno Mars", "video": "https://www.youtube.com/watch?v=Msl2fl3h59I"},
+        {"name": "Firework - Katy Perry", "video": "https://www.youtube.com/watch?v=QGJuMBdaqIw"}
     ],
 
     "sad": [
-        {
-            "name": "Someone Like You - Adele",
-            "video": "https://www.youtube.com/watch?v=hLQl3WQQoQ0"
-        },
-        {
-            "name": "Fix You - Coldplay",
-            "video": "https://www.youtube.com/watch?v=k4V3Mo61fJM"
-        },
-        {
-            "name": "Let Her Go - Passenger",
-            "video": "https://www.youtube.com/watch?v=RBumgq5yVrA"
-        }
+        {"name": "Someone Like You - Adele", "video": "https://www.youtube.com/watch?v=hLQl3WQQoQ0"},
+        {"name": "Let Her Go - Passenger", "video": "https://www.youtube.com/watch?v=RBumgq5yVrA"},
+        {"name": "Fix You - Coldplay", "video": "https://www.youtube.com/watch?v=k4V3Mo61fJM"},
+        {"name": "Photograph - Ed Sheeran", "video": "https://www.youtube.com/watch?v=nSDgHBxUbVQ"},
+        {"name": "Say Something - A Great Big World", "video": "https://www.youtube.com/watch?v=-2U0Ivkn2Ds"},
+        {"name": "All I Want - Kodaline", "video": "https://www.youtube.com/watch?v=mtf7hC17IBM"},
+        {"name": "When I Was Your Man - Bruno Mars", "video": "https://www.youtube.com/watch?v=ekzHIouo8Q4"},
+        {"name": "Memories - Maroon 5", "video": "https://www.youtube.com/watch?v=SlPhMPnQ58k"},
+        {"name": "Perfect - Ed Sheeran", "video": "https://www.youtube.com/watch?v=2Vv-BfVoq4g"},
+        {"name": "Happier - Marshmello", "video": "https://www.youtube.com/watch?v=m7Bc3pLyij0"}
     ],
 
     "angry": [
-        {
-            "name": "Believer - Imagine Dragons",
-            "video": "https://www.youtube.com/watch?v=7wtfhZwyrcc"
-        },
-        {
-            "name": "Numb - Linkin Park",
-            "video": "https://www.youtube.com/watch?v=kXYiU_JCYtU"
-        },
-        {
-            "name": "Stronger - Kanye West",
-            "video": "https://www.youtube.com/watch?v=PsO6ZnUZI0g"
-        }
+        {"name": "Believer - Imagine Dragons", "video": "https://www.youtube.com/watch?v=7wtfhZwyrcc"},
+        {"name": "Thunder - Imagine Dragons", "video": "https://www.youtube.com/watch?v=fKopy74weus"},
+        {"name": "Stronger - Kanye West", "video": "https://www.youtube.com/watch?v=PsO6ZnUZI0g"},
+        {"name": "Remember The Name - Fort Minor", "video": "https://www.youtube.com/watch?v=VDvr08sCPOc"},
+        {"name": "Warriors - Imagine Dragons", "video": "https://www.youtube.com/watch?v=fmI_Ndrxy14"},
+        {"name": "Radioactive - Imagine Dragons", "video": "https://www.youtube.com/watch?v=ktvTqknDobU"},
+        {"name": "Whatever It Takes - Imagine Dragons", "video": "https://www.youtube.com/watch?v=gOsM-DYAEhY"},
+        {"name": "Enemy - Imagine Dragons", "video": "https://www.youtube.com/watch?v=D9G1VOjN_84"},
+        {"name": "Legends Never Die", "video": "https://www.youtube.com/watch?v=r6zIGXun57U"},
+        {"name": "Hall Of Fame - The Script", "video": "https://www.youtube.com/watch?v=mk48xRzuNvA"}
     ],
 
     "neutral": [
-        {
-            "name": "Perfect - Ed Sheeran",
-            "video": "https://www.youtube.com/watch?v=2Vv-BfVoq4g"
-        },
-        {
-            "name": "Photograph - Ed Sheeran",
-            "video": "https://www.youtube.com/watch?v=nSDgHBxUbVQ"
-        },
-        {
-            "name": "Memories - Maroon 5",
-            "video": "https://www.youtube.com/watch?v=SlPhMPnQ58k"
-        }
-    ],
-
-    "surprise": [
-        {
-            "name": "Thunder - Imagine Dragons",
-            "video": "https://www.youtube.com/watch?v=fKopy74weus"
-        },
-        {
-            "name": "Counting Stars - OneRepublic",
-            "video": "https://www.youtube.com/watch?v=hT_nvWreIhg"
-        }
+        {"name": "Perfect - Ed Sheeran", "video": "https://www.youtube.com/watch?v=2Vv-BfVoq4g"},
+        {"name": "Count On Me - Bruno Mars", "video": "https://www.youtube.com/watch?v=Msl2fl3h59I"},
+        {"name": "Thinking Out Loud - Ed Sheeran", "video": "https://www.youtube.com/watch?v=lp-EO5I60KA"},
+        {"name": "A Thousand Years - Christina Perri", "video": "https://www.youtube.com/watch?v=rtOvBOTyX00"},
+        {"name": "Lucky - Jason Mraz", "video": "https://www.youtube.com/watch?v=acvIVA9-FMQ"},
+        {"name": "Yellow - Coldplay", "video": "https://www.youtube.com/watch?v=yKNxeF4KMsY"},
+        {"name": "Photograph - Ed Sheeran", "video": "https://www.youtube.com/watch?v=nSDgHBxUbVQ"},
+        {"name": "Stay With Me - Sam Smith", "video": "https://www.youtube.com/watch?v=pB-5XG-DbAA"},
+        {"name": "Let Her Go - Passenger", "video": "https://www.youtube.com/watch?v=RBumgq5yVrA"},
+        {"name": "Demons - Imagine Dragons", "video": "https://www.youtube.com/watch?v=mWRsgZuwf_8"}
     ],
 
     "fear": [
-        {
-            "name": "Lovely - Billie Eilish",
-            "video": "https://www.youtube.com/watch?v=V1Pl8CzNzCw"
-        },
-        {
-            "name": "Demons - Imagine Dragons",
-            "video": "https://www.youtube.com/watch?v=mWRsgZuwf_8"
-        }
+        {"name": "Demons - Imagine Dragons", "video": "https://www.youtube.com/watch?v=mWRsgZuwf_8"},
+        {"name": "Titanium - David Guetta", "video": "https://www.youtube.com/watch?v=JRfuAukYTKg"},
+        {"name": "Fight Song - Rachel Platten", "video": "https://www.youtube.com/watch?v=xo1VInw-SKc"},
+        {"name": "Brave - Sara Bareilles", "video": "https://www.youtube.com/watch?v=QUQsqBqxoR4"},
+        {"name": "Rise Up - Andra Day", "video": "https://www.youtube.com/watch?v=FBuIBaDSOa4"},
+        {"name": "Hall Of Fame", "video": "https://www.youtube.com/watch?v=mk48xRzuNvA"},
+        {"name": "Stronger", "video": "https://www.youtube.com/watch?v=PsO6ZnUZI0g"},
+        {"name": "Warriors", "video": "https://www.youtube.com/watch?v=fmI_Ndrxy14"},
+        {"name": "Believer", "video": "https://www.youtube.com/watch?v=7wtfhZwyrcc"},
+        {"name": "Unstoppable - Sia", "video": "https://www.youtube.com/watch?v=cxjvTXo9WWM"}
+    ],
+
+    "surprise": [
+        {"name": "Firework - Katy Perry", "video": "https://www.youtube.com/watch?v=QGJuMBdaqIw"},
+        {"name": "Happy - Pharrell Williams", "video": "https://www.youtube.com/watch?v=ZbZSe6N_BXs"},
+        {"name": "Levitating - Dua Lipa", "video": "https://www.youtube.com/watch?v=TUVcZfQe-Kw"},
+        {"name": "Good Time - Owl City", "video": "https://www.youtube.com/watch?v=H7HmzwI67ec"},
+        {"name": "Can't Stop The Feeling", "video": "https://www.youtube.com/watch?v=ru0K8uYEZWw"},
+        {"name": "Shake It Off - Taylor Swift", "video": "https://www.youtube.com/watch?v=nfWlot6h_JM"},
+        {"name": "Roar - Katy Perry", "video": "https://www.youtube.com/watch?v=CevxZvSJLk8"},
+        {"name": "On Top Of The World", "video": "https://www.youtube.com/watch?v=w5tWYmIOWGk"},
+        {"name": "Best Day Of My Life", "video": "https://www.youtube.com/watch?v=Y66j_BUCBMY"},
+        {"name": "Dynamite - BTS", "video": "https://www.youtube.com/watch?v=gdZLi9oWNZg"}
+    ],
+
+    "disgust": [
+        {"name": "Count On Me", "video": "https://www.youtube.com/watch?v=Msl2fl3h59I"},
+        {"name": "Perfect", "video": "https://www.youtube.com/watch?v=2Vv-BfVoq4g"},
+        {"name": "Photograph", "video": "https://www.youtube.com/watch?v=nSDgHBxUbVQ"},
+        {"name": "Yellow", "video": "https://www.youtube.com/watch?v=yKNxeF4KMsY"},
+        {"name": "Fix You", "video": "https://www.youtube.com/watch?v=k4V3Mo61fJM"},
+        {"name": "Memories", "video": "https://www.youtube.com/watch?v=SlPhMPnQ58k"},
+        {"name": "Rise Up", "video": "https://www.youtube.com/watch?v=FBuIBaDSOa4"},
+        {"name": "Fight Song", "video": "https://www.youtube.com/watch?v=xo1VInw-SKc"},
+        {"name": "Brave", "video": "https://www.youtube.com/watch?v=QUQsqBqxoR4"},
+        {"name": "Unstoppable", "video": "https://www.youtube.com/watch?v=cxjvTXo9WWM"}
     ]
 }
